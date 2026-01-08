a1e926bb2a1f6d1bedbb572f25307f4986afdd82
rasandy00 <137836649+rasandy00@users.noreply.github.com>
2026-01-08 16:57:17 +0300

    Add files via upload
    env de retour

--- src/parsing/struct_env.c
+++ src/parsing/struct_env.c
@@ -6,7 +6,7 @@
 /*   By: andriamr <andriamr@student.42antananari    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/05 16:46:21 by andriamr          #+#    #+#             */
-/*   Updated: 2026/01/08 09:59:52 by andriamr         ###   ########.fr       */
+/*   Updated: 2026/01/08 16:55:15 by andriamr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -74,10 +74,10 @@ void	init_env(t_cmd *cmd, char **envp)
 	int i;
 
 	i = 0;
-	cmd->env = NULL;
+	cmd->env_list = NULL;
 	while (envp[i])
 	{
-		add_list_env(cmd->env, envp[i]);
+		add_list_env(cmd->env_list, envp[i]);
 		i++;
 	}
 }
@@ -85,20 +85,35 @@ void	init_env(t_cmd *cmd, char **envp)
 t_cmd	*cpy_env_list(char **env)
 {
 	int i;
+	int len;
+	char **new_env;
 	t_cmd	*cmd_base;
 	t_env *tmp;
 	
 	i = 0;
+	len = 0;
+	while (env[len])
+		len++;
 	cmd_base = ft_calloc(sizeof(t_cmd), 1);
 	if (!cmd_base)
 		return (NULL);
 	tmp = NULL;
+	new_env = ft_calloc(sizeof (char *), len);
+	if (!new_env)
+		return (NULL);
+	len = 0;
+	while (env[len])
+	{
+		new_env[len] = ft_strdup(env[len]);
+		len++;
+	}
+	cmd_base->envp = new_env;
 	while (env[i])
 	{
 		add_list_env(tmp, env[i]);
 		i++;
 	}
 	// put_env(tmp);
-	cmd_base->env = tmp;
+	cmd_base->env_list = tmp;
 	return (cmd_base);
 }
