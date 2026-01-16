488fe7bc4c9121a0f5a3aa9f7cf6c36a9d5ef20f
rasandy00 <137836649+rasandy00@users.noreply.github.com>
2026-01-16 11:28:10 +0300

    Add files via upload

--- src/parsing/ft_list_str.c
+++ src/parsing/ft_list_str.c
@@ -6,7 +6,7 @@
 /*   By: andriamr <andriamr@student.42antananari    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/15 16:47:00 by andriamr          #+#    #+#             */
-/*   Updated: 2026/01/15 16:47:44 by andriamr         ###   ########.fr       */
+/*   Updated: 2026/01/16 11:24:45 by andriamr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,4 +38,57 @@ t_str	*add_str_last(t_str *list, char *str)
 		temp = temp->next;
 	temp->next = new;
 	return (list);
+}
+
+t_str  *char_to_list(char **str)
+{
+	t_str	*list;
+	int		i;
+
+	list = NULL;
+	i = 0;
+	while (str[i])
+	{
+		list = add_str_last(list, str[i]);
+		if (!list)
+			return (NULL);
+		i++;
+	}
+	return (list);
+}
+
+int	ft_list_size_str(t_str *list)
+{
+	t_str	*temp;
+	int		i;
+
+	temp = list;
+	i = 0;
+	while (temp)
+	{
+		i++;
+		temp = temp->next;
+	}
+	return (i);
+}
+
+char **list_to_char(t_str *list)
+{
+	char	**str;
+	t_str	*temp;
+	int		i;
+
+	str = ft_calloc(sizeof(char *), ft_list_size_str(list) + 1);
+	if (!str)
+		return (NULL);
+	temp = list;
+	i = 0;
+	while (temp)
+	{
+		str[i] = temp->str;
+		temp = temp->next;
+		i++;
+	}
+	str[i] = NULL;
+	return (str);
 }
